17e08cba121ad2d3ec7eca13c4abfc36327e0fb1
bhavish-programmer <bhavishdomale@gmail.com>
2025-07-04 14:53:18 +0530

    Add Success

---
+++ JAVASCRIPT Bhavish/Level-1/script.js
@@ -0,0 +1,4 @@
+console.log('I am Comming From Script.js')
+console.clear()  
+// This is a Comments.
+// Hello Brother , how are you ? fine or not? All Subject is Clear or Not? But Dont's Flear When Bhavish is Here! 
